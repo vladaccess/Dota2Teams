dd4973597cf0232444808c5cb18c87f2f4318be1
Vlad Krasnokutskii <vladaccess@outlook.com>
2019-03-10 16:03:33 +0300

    Initial Commit

---
+++ Dota2Teams/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dota2Teams
+//
+//  Created by Liquid on 10/03/2019.
+//  Copyright © 2019 attom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
